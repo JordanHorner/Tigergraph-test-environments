12db70a9c71bc4201e23089bdc051a8c2c3aba2a
JordanHorner <89211825+JordanHorner@users.noreply.github.com>
2022-11-07 14:23:51 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,10 @@
-sudo su - tigergraph bash -c "export PATH=/home/tigergraph/tigergraph/app/cmd:\$PATH; bash /home/tigergraph/config.sh tail -f /dev/null"
+#! /bin/bash
+[ -f /home/tigergraph/.ssh/sshd_rsa ] || ssh-keygen -b 4096 -t rsa -f /home/tigergraph/.ssh/sshd_rsa -q -N ''
+if [ ! -f /home/tigergraph/.ssh/tigergraph_rsa ]; then
+  ssh-keygen -b 4096 -t rsa -f /home/tigergraph/.ssh/tigergraph_rsa -q -N ''
+  cat /home/tigergraph/.ssh/tigergraph_rsa.pub > /home/tigergraph/.ssh/authorized_keys
+fi
+sudo /usr/sbin/sshd -h /home/tigergraph/.ssh/sshd_rsa
+bash /home/tigergraph/config.sh 
+/home/tigergraph/tigergraph/app/cmd/gadmin start all --auto-restart
+tail -f /dev/null

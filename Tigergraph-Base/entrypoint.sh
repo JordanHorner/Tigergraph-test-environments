36c7e95b71e8b73c974dd210312f65ce59c36b7f
Jordan <jordan.horner@tigergraph.com>
2022-11-10 01:19:57 -0500

    fix(entrypoint): add to each directory

---
+++ Tigergraph-Base/entrypoint.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+[ -f /home/tigergraph/.ssh/sshd_rsa ] || ssh-keygen -b 4096 -t rsa -f /home/tigergraph/.ssh/sshd_rsa -q -N ''
+if [ ! -f /home/tigergraph/.ssh/tigergraph_rsa ]; then
+  ssh-keygen -b 4096 -t rsa -f /home/tigergraph/.ssh/tigergraph_rsa -q -N ''
+  cat /home/tigergraph/.ssh/tigergraph_rsa.pub > /home/tigergraph/.ssh/authorized_keys
+fi
+sudo /usr/sbin/sshd -h /home/tigergraph/.ssh/sshd_rsa
+/home/tigergraph/tigergraph/app/cmd/gadmin start all --auto-restart
+tail -f /dev/null
